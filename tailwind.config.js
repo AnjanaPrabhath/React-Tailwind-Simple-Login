b5dc0dc9b6164d0cfe50067fb95f3b2e4c5fa459
AnjanaPrabhath <prabhathxdissanayake@gmail.com>
2024-01-30 00:02:55 +0530

    login page

---
+++ tailwind.config.js
@@ -0,0 +1,14 @@
+// TW Elements is free under AGPL, with commercial license required for specific uses. See more details: https://tw-elements.com/license/ and contact us for queries at tailwind@mdbootstrap.com 
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./index.html",
+    "./src/**/*.{js,ts,jsx,tsx}",
+    "./node_modules/tw-elements/dist/js/**/*.js",
+  ],
+  theme: {
+    extend: {},
+  },
+  darkMode: "class",
+  plugins: [require("tw-elements/dist/plugin.cjs")],
+};
